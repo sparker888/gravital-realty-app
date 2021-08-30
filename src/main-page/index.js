5a3ca478a2db29446f88744e9f31890f14a499b7
Stephen Parker <sparker@gravitaldigital.com>
2021-08-30 04:57:13 -0400

    refactor into hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,31 +1,16 @@
-import { useState, useEffect, useMemo } from "react";
 import "./main-page.css";
 import Header from "./header";
 import FeaturedHouse from "./featured-house";
 import HouseFilter from "./house-filter";
 import SearchResults from "../search-results";
-
+import useHouses from "../hooks/useHouses";
+import useFeaturedHouse from "../hooks/useFeaturedHouse";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HouseFromQuery from "../house/HouseFromQuery";
 
 function App() {
-  const [allHouses, setAllHouses] = useState([]);
-
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const rsp = await fetch("/houses.json");
-      const houses = await rsp.json();
-      setAllHouses(houses);
-    };
-    fetchHouses();
-  }, []);
-
-  const featuredHouse = useMemo(() => {
-    if (allHouses.length) {
-      const randomIndex = Math.floor(Math.random() * allHouses.length);
-      return allHouses[randomIndex];
-    }
-  }, [allHouses]);
+  const allHouses = useHouses();
+  const featuredHouse = useFeaturedHouse(allHouses);
 
   return (
     <Router>
